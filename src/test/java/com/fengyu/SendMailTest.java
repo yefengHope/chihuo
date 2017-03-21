fc144c622d3a8fd306a06703b6bda89129405fa0
yefeng <1825445185@qq.com>
2017-03-21 22:13:50 +0800

    邮件模块 - 测试并发送成功

---
+++ src/test/java/com/fengyu/SendMailTest.java
@@ -0,0 +1,31 @@
+package com.fengyu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 邮件测试
+ * Created by rain on 2017/3/21.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ChihuoApplication.class)
+public class SendMailTest {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Test
+    public void sendSimpleMail() throws Exception {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("15881253374@163.com");   // 发送者
+        message.setTo("15881253374@163.com");     // 接收者
+        message.setSubject("主题：简单邮件");      //邮件主题
+        message.setText("测试邮件内容");          //邮件内容
+        mailSender.send(message);               //发送邮件
+    }
+}
