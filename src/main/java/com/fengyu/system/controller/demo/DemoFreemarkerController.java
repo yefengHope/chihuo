2acfa7f6f097415d846b8a2177630d22962a00ab
yefeng <1825445185@qq.com>
2017-07-13 09:37:05 +0800

    【freemarker Demo】：freemarker样例 - list

--- src/main/java/com/fengyu/system/controller/demo/DemoFreemarkerController.java
+++ src/main/java/com/fengyu/system/controller/demo/DemoFreemarkerController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <p>@Title 类标题描述 </p>
  * <p>@Description 类功能描述（功能，作用）,描述过多时可以换行</p>
@@ -27,4 +30,24 @@ public class DemoFreemarkerController {
         model.addAttribute("latestProduct",json);
         return "demo/ifPage";
     }
+
+
+    @RequestMapping(value = "list")
+    public String tolist(Model model) {
+        model.addAttribute("user","李逵");
+        List<JSONObject> list = new ArrayList<>();
+
+        JSONObject json1 = new JSONObject();
+        json1.put("name","余萍");
+        json1.put("price","￥2.0");
+        list.add(json1);
+
+        JSONObject json2 =new JSONObject();
+        json2.put("name","张瑞");
+        json2.put("price","￥2.1");
+        list.add(json2);
+        model.addAttribute("animals",list);
+        return "demo/listPage";
+    }
+
 }
