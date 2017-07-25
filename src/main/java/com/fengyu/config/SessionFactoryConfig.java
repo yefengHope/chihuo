2e780924dfda32ab0b2bd85e6846a30512fe6f67
hanfengHope <1825445185@qq.com>
2017-07-25 11:38:23 +0800

    【SqlSessionFactoryBean异常】：因为创建了两个工厂，一个在mybatis自动扫描中，一个是SessionFactoryConfig.java

--- src/main/java/com/fengyu/config/SessionFactoryConfig.java
+++ src/main/java/com/fengyu/config/SessionFactoryConfig.java
@@ -1,22 +1,14 @@
 package com.fengyu.config;
 
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
-
 import javax.annotation.Resource;
 import javax.sql.DataSource;
-import java.io.IOException;
 
 /**
  * mybatis 扫描路径设置
  * Created by rain on 2017/3/13.
  */
 
-@Configuration
+// @Configuration
 public class SessionFactoryConfig {
 
 
@@ -39,22 +31,23 @@ public class SessionFactoryConfig {
      * 并且设置configtion 如驼峰命名.等等
      * 设置mapper 映射路径
      * 设置datasource数据源
+     * @deprecated 新配置在MybatisAutoConfiguration.java
      * @return
      */
-    @Bean
-    public SqlSessionFactoryBean createSqlSessionFactoryBean() throws IOException {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        // 设置mybatis configuration 扫描路径
-        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(MYBATIS_CONFIG));
-        // 添加mapper 扫描路径
-        PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver = new PathMatchingResourcePatternResolver();
-        String packageSearchPath = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + MAPPER_PATH;
-        sqlSessionFactoryBean.setMapperLocations(pathMatchingResourcePatternResolver.getResources(packageSearchPath));
-        // 设置datasource
-        sqlSessionFactoryBean.setDataSource(dataSource);
-        // 设置typeAlias 包扫描路径
-        sqlSessionFactoryBean.setTypeAliasesPackage(typeAliasPackage);
-        return sqlSessionFactoryBean;
-    }
+    // @Bean
+    // public SqlSessionFactoryBean createSqlSessionFactoryBean() throws IOException {
+    //     SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+    //     // 设置mybatis configuration 扫描路径
+    //     sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(MYBATIS_CONFIG));
+    //     // 添加mapper 扫描路径
+    //     PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver = new PathMatchingResourcePatternResolver();
+    //     String packageSearchPath = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + MAPPER_PATH;
+    //     sqlSessionFactoryBean.setMapperLocations(pathMatchingResourcePatternResolver.getResources(packageSearchPath));
+    //     // 设置datasource
+    //     sqlSessionFactoryBean.setDataSource(dataSource);
+    //     // 设置typeAlias 包扫描路径
+    //     sqlSessionFactoryBean.setTypeAliasesPackage(typeAliasPackage);
+    //     return sqlSessionFactoryBean;
+    // }
 
 }
