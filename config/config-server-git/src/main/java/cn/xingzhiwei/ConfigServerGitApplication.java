6d70ee13ec6b5872583d069d52d3c3bfbd1fa7e8
hanfengHope <1825445185@qq.com>
2018-01-24 23:14:59 +0800

    【xingzhiwei】-【配置git】：仓库

--- config/config-server-git/src/main/java/cn/xingzhiwei/ConfigServerGitApplication.java
+++ config/config-server-git/src/main/java/cn/xingzhiwei/ConfigServerGitApplication.java
@@ -2,7 +2,9 @@ package cn.xingzhiwei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer // 开启配置中心
 @SpringBootApplication
 public class ConfigServerGitApplication {
 
